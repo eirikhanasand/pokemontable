1eb33daab79344306ee775eef885e95751478fde
eirikhanasand <eirik.hanasand@gmail.com>
2023-10-04 16:53:50 +0200

    Updated layout

--- src/components/pokemon.tsx
+++ src/components/pokemon.tsx
@@ -7,8 +7,13 @@ import { useEffect } from "react"
 import extractImportantData from "../utils/extractImportantData"
 import Loading from "./loading"
 
-type GeneralInfoProps = {
+type PokemonObject = {
     pokemon: Pokemon
+}
+
+type StatsObject = {
+    title: string
+    value: string | number
 }
 
 export default function Pokemon(): JSX.Element {
@@ -40,44 +45,73 @@ export default function Pokemon(): JSX.Element {
     return (
         <div className="wrapper">
             <div className="pokemon_background">
-            <PokemonHeader />
-            {/* <div className="line" /> */}
-            <div className="table_content">
-                <img className="pokemon_image" 
-                    src={item.image} 
-                    alt={item.name}
-                />
-                <GeneralInfo pokemon={item} />
-        </div>
+                <PokemonHeader />
+                <div className="title_view">
+                    <img className="pokemon_image" 
+                        src={item.image} 
+                        alt={item.name}
+                    />
+                    <GeneralInfo pokemon={item} />
+                    <Species pokemon={item} />
+                </div>
+                <div className="title_view">
+                    
+                    <Stats pokemon={item} />
+                    <div className="blue"></div>
+                </div>
         </div>
         </div>
     )
 }
 
-function GeneralInfo({pokemon}: GeneralInfoProps): JSX.Element {
+function GeneralInfo({pokemon}: PokemonObject): JSX.Element {
     return (
         <div className="general_info">
             <p className="general_name">{pokemon.name} · {pokemon.id}</p>
             <div>
-                <p className="pokemon_item">Generation: {pokemon.generation}</p>
-                <p className="pokemon_item">Region name: {pokemon.region}</p>
-                {/* <p className="pokemon_item">{pokemon.weight}</p>
-                <p className="pokemon_item">{pokemon.height}</p>
-                <p className="pokemon_item">HP: {pokemon.hp}</p>
-                <p className="pokemon_item">Attack: {pokemon.attack}</p>
-                <p className="pokemon_item">Defense: {pokemon.defense}</p>
-                <p className="pokemon_item">Special attack: {pokemon.special_attack}</p>
-                <p className="pokemon_item">Special defense: {pokemon.special_defense}</p>
-                <p className="pokemon_item">Speed: {pokemon.speed}</p>
-                <p className="pokemon_item">Species: {pokemon.species}</p> */}
+                <p className="pokemon_item">{pokemon.generation} · {pokemon.region}</p>
             </div>
-            {/* <p className="pokemon_item">
-                {pokemon.types.map((type) => type + "\n")} 
-            </p> */}
         </div>
     )
 }
 
+function Stats({pokemon}: PokemonObject): JSX.Element {
+    return (
+        <div className="stats">
+            <div >
+                <StatsObject title="Weight:" value={pokemon.weight} />
+                <StatsObject title="Height:" value={pokemon.height} />
+                <StatsObject title="HP:" value={pokemon.hp} />
+                <StatsObject title="Attack:" value={pokemon.attack} />
+            </div>
+            <div >
+                <StatsObject title="Defense:" value={pokemon.defense} />
+                <StatsObject title="Special attack:" value={pokemon.special_attack} />
+                <StatsObject title="Special defense:" value={pokemon.special_defense} />
+                <StatsObject title="Speed:" value={pokemon.speed} />
+            </div>
+        </div>
+    )
+}
+
+function StatsObject({title, value}: StatsObject): JSX.Element {
+    return (
+        <div className="stats_object">
+            <p className="stats_object_item">{title}</p>
+            <p className="pokemon_item">{value}</p>
+        </div>
+    )
+}
+
+function Species({pokemon}: PokemonObject): JSX.Element {
+
+    return (
+        <div className="species">
+            <StatsObject title="Species:" value={pokemon.species} />
+            <StatsObject title="Type:" value={pokemon.types.join(", ")} />
+        </div>
+    )
+}
 
 // It should be possible for a user to obtain detailed stats(hp, defence, attack etc) about each pokemon in the table by selecting it. 
 // The detailed information about the selected pokemon should be displayed in a suitable manner. 
